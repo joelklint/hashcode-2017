f347f2e16528112831767d69499547965eba0f88
Joel Klint <joel.klint@gmail.com>
2017-02-23 20:35:22 +0100

    fixed thing

--- src/main/java/hashcode/CacheServer.java
+++ src/main/java/hashcode/CacheServer.java
@@ -6,13 +6,35 @@ public class CacheServer {
 	
 	public int id;
 	public int capacity;
+	private int capacityLeft;
 	public ArrayList<Video> videos;
+	private int failedInputAttempts = 0;
+	private boolean full = false;
 	
 	public CacheServer(int id, int capacity) {
 		this.id = id;
 		this.capacity = capacity;
+		this.capacityLeft = capacity;
 		this.videos = new ArrayList<>();
 		
+	}
+	
+	public boolean add(Video video) {
+		if(capacityLeft >= video.size) {
+			videos.add(video);
+			return true;
+		}
+		else {
+			failedInputAttempts++;
+			if(failedInputAttempts > 5) {
+				this.full = true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isFull() {
+		return full;
 	}
 			
 			
